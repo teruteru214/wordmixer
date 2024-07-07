5d9b67dc8cd0be611437ba0b6a571fd6640bb8d6
teru <85771338+teruteru214@users.noreply.github.com>
2024-07-07 17:37:21 +0900

    Style: LoginHeaderのデザインの改修

--- src/components/Ui/Drawer.tsx
+++ src/components/Ui/Drawer.tsx
@@ -41,7 +41,7 @@ const DrawerContent = React.forwardRef<
 		<DrawerPrimitive.Content
 			ref={ref}
 			className={cn(
-				"fixed inset-y-0 right-0 z-50 flex sm:w-96 w-56 flex-col border bg-background",
+				"fixed inset-y-0 right-0 z-50 flex sm:w-96 w-60 flex-col border bg-background",
 				className,
 			)}
 			{...props}
