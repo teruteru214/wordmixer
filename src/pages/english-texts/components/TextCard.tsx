fd87eebeec0624d9d285179fcec9165c1cb79837
teru <85771338+teruteru214@users.noreply.github.com>
2024-07-08 13:00:12 +0900

    Fix: textオブジェクトがnullやundefinedも許容するように

--- src/pages/english-texts/components/TextCard.tsx
+++ src/pages/english-texts/components/TextCard.tsx
@@ -14,21 +14,21 @@ export default function TextCard({ text }: TextProps) {
 	return (
 		<Card className="my-4">
 			<CardHeader>
-				<CardTitle>{text.english}</CardTitle>
+				<CardTitle>{text?.english}</CardTitle>
 			</CardHeader>
 			<hr />
 			<CardContent className="space-y-1">
 				<div className="flex flex-wrap gap-2 mt-2 text-gray-400">
-					{text.words.map((word) => (
+					{text?.words.map((word) => (
 						<Badge key={word}>{word}</Badge>
 					))}
 				</div>
-				<p className="text-sm text-gray-400">{text.level}</p>
+				<p className="text-sm text-gray-400">{text?.level}</p>
 				<div className="flex items-center justify-between text-gray-400 text-sm">
-					<p>{text.theme}</p>
+					<p>{text?.theme}</p>
 					<div className="flex items-center space-x-2">
-						<Checkbox id={`terms-${text.id}`} />
-						<label htmlFor={`terms-${text.id}`}>あとで翻訳</label>
+						<Checkbox id={`terms-${text?.id}`} />
+						<label htmlFor={`terms-${text?.id}`}>あとで翻訳</label>
 						<IconLanguageHiragana className="w-5 h-5 text-gray-400" />
 						<IconThumbUp className="w-5 h-5 text-gray-400" />
 						<IconTrash className="w-5 h-5 text-gray-400" />
