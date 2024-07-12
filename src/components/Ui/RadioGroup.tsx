8bf76ae9b85c9bb3a94cae7ad81d586882660b2a
teru <85771338+teruteru214@users.noreply.github.com>
2024-07-12 20:37:31 +0900

    Style: Formコンポーネントの作成

--- src/components/Ui/RadioGroup.tsx
+++ src/components/Ui/RadioGroup.tsx
@@ -26,13 +26,13 @@ const RadioGroupItem = React.forwardRef<
 		<RadioGroupPrimitive.Item
 			ref={ref}
 			className={cn(
-				"aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+				"aspect-square h-5 w-5 rounded-full border border-blue-500 text-blue-500 ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
 				className,
 			)}
 			{...props}
 		>
 			<RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-				<Circle className="h-2.5 w-2.5 fill-current text-current" />
+				<Circle className="h-3 w-3 rounded-full bg-blue-500" />
 			</RadioGroupPrimitive.Indicator>
 		</RadioGroupPrimitive.Item>
 	);
