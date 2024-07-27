7004ea053047ed547181ad1e8fe1cd5c1993af60
teru <85771338+teruteru214@users.noreply.github.com>
2024-07-27 13:34:24 +0900

    Feat: 新規登録とログインモーダルにバリテーションを追加+auth.userがsupabaseに登録されるように

--- src/components/Layout/Header/RegisterModal.tsx
+++ src/components/Layout/Header/RegisterModal.tsx
@@ -1,4 +1,15 @@
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/Ui/Form";
+import { supabase } from "@/lib/supabase";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { IconBrandGoogle, IconX } from "@tabler/icons-react";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
 import {
 	AlertDialog,
 	AlertDialogCancel,
@@ -11,7 +22,57 @@ import {
 import { Button } from "../../Ui/Button";
 import { Input } from "../../Ui/Input";
 
+const schema = z
+	.object({
+		name: z.string().nonempty({ message: "名前を入力してください" }),
+		email: z
+			.string()
+			.nonempty({ message: "メールアドレスを入力してください" })
+			.email({ message: "有効なメールアドレスを入力してください" }),
+		password: z
+			.string()
+			.min(8, { message: "パスワードは8文字以上で入力してください" }),
+		confirmPassword: z
+			.string()
+			.min(8, { message: "確認パスワードは8文字以上で入力してください" }),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "パスワードが一致しません",
+		path: ["confirmPassword"],
+	});
+
+type FormData = z.infer<typeof schema>;
+
 const RegisterModal = () => {
+	const methods = useForm<FormData>({
+		resolver: zodResolver(schema),
+	});
+
+	const {
+		handleSubmit,
+		formState: { errors, isValid },
+		reset,
+	} = methods;
+
+	const onSubmit = async (data: FormData) => {
+		try {
+			const { email, password } = data;
+			const { error } = await supabase.auth.signUp({
+				email,
+				password,
+			});
+
+			if (error) {
+				throw new Error(error.message);
+			}
+			console.log("Registration successful");
+		} catch (err) {
+			console.error(err);
+		} finally {
+			reset();
+		}
+	};
+
 	return (
 		<AlertDialog>
 			<AlertDialogTrigger asChild>
@@ -26,25 +87,105 @@ const RegisterModal = () => {
 						<IconX />
 					</AlertDialogCancel>
 				</AlertDialogHeader>
-				<Input className="rounded" placeholder="メールアドレス" />
-				<Input className="rounded" placeholder="パスワード" />
-				<Input className="rounded" placeholder="パスワード(確認)" />
-				<AlertDialogDescription>
-					利用規約およびプライバシーポリシーに同意した上で、
-					以下の「登録」ボタンを押してください。
-				</AlertDialogDescription>
-				<div className="flex flex-col space-y-2">
-					<Button>新規登録</Button>
-					<div className="flex items-center">
-						<hr className="flex-grow border-gray-300" />
-						<p className="px-2 text-gray-400">または</p>
-						<hr className="flex-grow border-gray-300" />
-					</div>
-					<Button>
-						<IconBrandGoogle className="mr-2" />
-						Googleアカウントで新規登録
-					</Button>
-				</div>
+				<FormProvider {...methods}>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<FormField
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input
+											className="rounded"
+											placeholder="john doe"
+											{...field}
+										/>
+									</FormControl>
+									{errors.name && (
+										<FormMessage>{errors.name.message}</FormMessage>
+									)}
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											className="rounded"
+											placeholder="example@gmail.com"
+											{...field}
+										/>
+									</FormControl>
+									{errors.email && (
+										<FormMessage>{errors.email.message}</FormMessage>
+									)}
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											className="rounded"
+											placeholder="8 characters or more"
+											{...field}
+										/>
+									</FormControl>
+									{errors.password && (
+										<FormMessage>{errors.password.message}</FormMessage>
+									)}
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							name="confirmPassword"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Confirm Password</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											className="rounded"
+											placeholder="confirmation"
+											{...field}
+										/>
+									</FormControl>
+									{errors.confirmPassword && (
+										<FormMessage>{errors.confirmPassword.message}</FormMessage>
+									)}
+								</FormItem>
+							)}
+						/>
+
+						<AlertDialogDescription>
+							利用規約およびプライバシーポリシーに同意した上で、以下の「登録」ボタンを押してください。
+						</AlertDialogDescription>
+						<div className="flex flex-col space-y-2">
+							<Button type="submit" variant={isValid ? "default" : "disabled"}>
+								新規登録
+							</Button>
+							<div className="flex items-center">
+								<hr className="flex-grow border-gray-300" />
+								<p className="px-2 text-gray-400">または</p>
+								<hr className="flex-grow border-gray-300" />
+							</div>
+							<Button>
+								<IconBrandGoogle className="mr-2" />
+								Googleアカウントで新規登録
+							</Button>
+						</div>
+					</form>
+				</FormProvider>
 				<AlertDialogDescription className="text-center">
 					登録済みの方はこちら
 				</AlertDialogDescription>
