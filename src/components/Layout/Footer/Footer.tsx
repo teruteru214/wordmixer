4294f928216e2ab2ee8c6ef598ebc851382489a1
teru <85771338+teruteru214@users.noreply.github.com>
2024-08-15 16:05:01 +0900

    Refactor: 親要素の横幅指定をやめ、それぞれのコンポーネントで管理する

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -4,29 +4,33 @@ import logo from "../../../public/logo.webp";
 const Footer = () => {
 	return (
 		<footer className="p-2 border-t border-gray-200 sm:px-10">
-			<div className="flex justify-between">
-				<menu className="text-left text-xs sm:text-base">
-					<li className="text-gray-400 hover:underline">本サービスについて</li>
-				</menu>
-				<menu className="text-left text-xs sm:text-base">
-					<li className="text-gray-400 hover:underline">利用規約</li>
-					<li className="text-gray-400 hover:underline">
-						プライバシーポリシー
-					</li>
-					<li className="text-gray-400 hover:underline">お問い合わせ</li>
-				</menu>
-				<menu className="text-left text-xs sm:text-base">
-					<li className="text-gray-400 hover:underline">Xアカウント</li>
-					<li className="text-gray-400 hover:underline">Github</li>
-					<li className="text-gray-400 hover:underline">Qiita記事</li>
-				</menu>
+			<div className="max-w-7xl mx-auto">
+				<div className="flex justify-between">
+					<menu className="text-left text-xs sm:text-base">
+						<li className="text-gray-400 hover:underline">
+							本サービスについて
+						</li>
+					</menu>
+					<menu className="text-left text-xs sm:text-base">
+						<li className="text-gray-400 hover:underline">利用規約</li>
+						<li className="text-gray-400 hover:underline">
+							プライバシーポリシー
+						</li>
+						<li className="text-gray-400 hover:underline">お問い合わせ</li>
+					</menu>
+					<menu className="text-left text-xs sm:text-base">
+						<li className="text-gray-400 hover:underline">Xアカウント</li>
+						<li className="text-gray-400 hover:underline">Github</li>
+						<li className="text-gray-400 hover:underline">Qiita記事</li>
+					</menu>
+				</div>
+				<div className="flex justify-center my-3">
+					<Image src={logo} alt="logo" height={50} />
+				</div>
+				<p className="text-xs sm:text-base text-center text-gray-400 mb-3">
+					@2024 WordMixer All Rights
+				</p>
 			</div>
-			<div className="flex justify-center my-3">
-				<Image src={logo} alt="logo" height={50} />
-			</div>
-			<p className="text-xs sm:text-base text-center text-gray-400 mb-3">
-				@2024 WordMixer All Rights
-			</p>
 		</footer>
 	);
 };
