038a257d6e73ee56b28be23e921de93dadb360c7
teru <85771338+teruteru214@users.noreply.github.com>
2024-08-17 11:34:31 +0900

    Style: HeroContentのレスポンシブ調整

--- src/components/Ui/HeroContent.tsx
+++ src/components/Ui/HeroContent.tsx
@@ -7,13 +7,13 @@ interface HeroContentProps {
 
 const HeroContent = ({ icon, title }: HeroContentProps) => {
 	return (
-		<div className="h-[150px]">
+		<div className="h-[100px] sm:h-[150px]">
 			<div className="bg-blue-100 w-full h-full">
-				<div className="max-w-5xl mx-auto flex items-center p-4 justify-start h-full">
-					<div className="text-2xl mr-3 bg-white rounded-lg w-[100px] h-[100px] flex items-center justify-center">
+				<div className="max-w-5xl mx-auto flex items-center p-2 sm:p-6 justify-start h-full">
+					<div className="text-2xl mr-3 bg-white rounded-lg w-[60px] h-[60px] sm:w-[100px] sm:h-[100px] flex items-center justify-center">
 						{icon}
 					</div>
-					<h1 className="font-bold text-3xl ml-4">{title}</h1>
+					<h1 className="font-bold text-lg sm:text-2xl sm:ml-4">{title}</h1>
 				</div>
 			</div>
 		</div>
