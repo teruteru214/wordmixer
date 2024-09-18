f69b9cec2cece85aa88651162669f1da1ed566b2
teru <85771338+teruteru214@users.noreply.github.com>
2024-09-18 16:37:38 +0900

    Fix: クライアントのみでuseEffectのコードが行われるように設定

--- src/pages/texts/[id]/components/Material.tsx
+++ src/pages/texts/[id]/components/Material.tsx
@@ -17,52 +17,54 @@ const Material = ({ words, level, theme }: MaterialProps) => {
 		[key: string]: string;
 	}>({});
 
-	useEffect(() => {
-		const translateWords = async () => {
-			try {
-				const response = await fetch("/api/texts/words", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ words }),
-				});
+	if (typeof window !== "undefined") {
+		useEffect(() => {
+			const translateWords = async () => {
+				try {
+					const response = await fetch("/api/texts/words", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ words }),
+					});
 
-				const data = await response.json();
+					const data = await response.json();
 
-				const translations = words.reduce(
-					(acc, word, index) => {
-						acc[word] = data.translations[index].text;
-						return acc;
-					},
-					{} as { [key: string]: string },
-				);
+					const translations = words.reduce(
+						(acc, word, index) => {
+							acc[word] = data.translations[index].text;
+							return acc;
+						},
+						{} as { [key: string]: string },
+					);
 
-				setTranslatedWords(translations);
-			} catch (error) {
-				console.error("Error fetching translation:", error);
-			}
-		};
+					setTranslatedWords(translations);
+				} catch (error) {
+					console.error("Error fetching translation:", error);
+				}
+			};
 
-		translateWords();
-	}, [words]);
+			translateWords();
+		}, [words]);
+	}
 
 	return (
 		<Accordion type="single" collapsible className="w-full">
 			<AccordionItem value="item-1">
 				<AccordionTrigger>使われた英単語・レベル・テーマ</AccordionTrigger>
 				<AccordionContent>
-					<ul className="ml-2 space-y-2">
-						<li>
-							<strong>英単語:</strong>
-						</li>
+					<ul className="ml-2 space-y-4">
+						<li className="text-xl font-bold">英単語</li>
 						{words.map((word) => (
 							<li key={word}>
 								{word}: {translatedWords[word] || "翻訳中..."}
 							</li>
 						))}
-						<li>レベル: {level}</li>
-						<li>テーマ: {theme}</li>
+						<li className="text-xl font-bold">レベル</li>
+						<li>{level}</li>
+						<li className="text-xl font-bold">テーマ</li>
+						<li>{theme}</li>
 					</ul>
 				</AccordionContent>
 			</AccordionItem>
